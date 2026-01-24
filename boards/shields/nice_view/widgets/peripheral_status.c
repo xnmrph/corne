9153210c2e2fa58f683f966b6dbe6d2f185d023d
xnmrph <social@xenomxr.ph>
2026-01-24 17:43:33 +0100

    Fix image source assignment in peripheral_status.c

--- boards/shields/nice_view/widgets/peripheral_status.c
+++ boards/shields/nice_view/widgets/peripheral_status.c
@@ -117,7 +117,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     bool random = sys_rand32_get() & 1;
-    lv_image_set_src(art, random ? &balloon : &mountain);
+    lv_image_set_src(art, random ? &balloon : &mountain &mxLabs_pixel_1bit_68x140);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
